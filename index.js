270804ac1ad49de814c98259aac1e6c4c99768bf
MisterGuy2013 <90930048+MisterGuy2013@users.noreply.github.com>
2021-09-17 21:26:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,8 +14,15 @@ var unblocker = new Unblocker({
   ]
 });
 
-
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
 // ...
+app.use(requireHTTPS);
 app.use(unblocker);
 
 app.use('/', express.static(__dirname + '/public'));
